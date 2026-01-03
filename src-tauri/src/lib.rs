d2180ef610dc2511917dfc9fd4a7798e95745e93
Laurenz Hieber <laurenzhieber99@gmail.com>
2026-01-03 18:12:45 +0100

    test

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,7 +4,6 @@ mod ski_area;
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .plugin(tauri_plugin_geolocation::init())
         .invoke_handler(tauri::generate_handler![ski_area::fetch_ski_areas])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
